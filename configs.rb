c3f3b69863b3172aa02c4cf5f20d05e8c69df06f
Ivan Kusalic <ikusalic@gmail.com>
2012-10-05 13:57:34 +0200

    Update cloudControl-specific constants

--- configs.rb
+++ configs.rb
@@ -18,16 +18,20 @@ module Configs
   BUNDLER_GEM_PATH    = "bundler-#{BUNDLER_VERSION}"
   NODE_VERSION        = "0.4.7"
   NODE_JS_BINARY_PATH = "node-#{NODE_VERSION}"
-  JVM_VERSION         = "openjdk7-latest"
 
   # vendor specific constants
   if paas_vendor == :heroku
     S3_BUCKET_NAME = "heroku-buildpack-ruby"
     VENDOR_URL     = "https://s3.amazonaws.com/#{S3_BUCKET_NAME}"
+    JVM_VERSION    = "openjdk7-latest"
     JVM_BASE_URL   = "http://heroku-jvm-langpack-java.s3.amazonaws.com"
   elsif paas_vendor == :cloudcontrol
-    S3_BUCKET_NAME = "heroku-buildpack-ruby"
-    VENDOR_URL     = "https://s3.amazonaws.com/#{S3_BUCKET_NAME}"
-    JVM_BASE_URL   = "https://s3-eu-west-1.amazonaws.com/cloudcontrolled.com.packages"
+    S3_BASE        = "https://s3-eu-west-1.amazonaws.com"
+    S3_BUCKET_NAME = "#{ ENV["DOMAIN"] }.packages"
+    S3_RUBY_DIR    = "buildpack-ruby"
+    S3_JAVA_DIR    = "buildpack-java"
+    JVM_VERSION    = "openjdk7"
+    VENDOR_URL     = "#{S3_BASE}/#{S3_BUCKET_NAME}/#{S3_RUBY_DIR}"
+    JVM_BASE_URL   = "#{S3_BASE}/#{S3_BUCKET_NAME}/#{S3_JAVA_DIR}"
   end
 end
